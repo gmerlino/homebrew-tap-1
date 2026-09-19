9ef49ffd6b93d1bc5b108dce505b43370e9c9951
Oscar Spencer <oscar@grain-lang.org>
2026-09-19 13:04:31 -0400

    feat: Support ARM

--- Casks/grain.rb
+++ Casks/grain.rb
@@ -1,16 +1,15 @@
 cask "grain" do
-  version "0.7.2"
-  sha256 "f7d53bf0b59d2eecf55dcb7df474d2bcf501aa5c83c74965a0ae57b2f9336750"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/grain-lang/grain/releases/download/grain-v#{version}/grain-mac-x64",
+  version "0.7.2"
+  sha256 arm:   "f7d53bf0b59d2eecf55dcb7df474d2bcf501aa5c83c74965a0ae57b2f9336750",
+         intel: "f7d53bf0b59d2eecf55dcb7df474d2bcf501aa5c83c74965a0ae57b2f9336750"
+
+  url "https://github.com/grain-lang/grain/releases/download/grain-v#{version}/grain-mac-#{arch}.tar.gz",
       verified: "github.com/grain-lang/grain/"
   name "The Grain Programming Language"
   desc "Packaged Grain binary, including the compiler, runtime & stdlib"
   homepage "https://grain-lang.org/"
 
-  binary "grain-mac-x64", target: "grain"
-
-  caveats do
-    requires_rosetta
-  end
+  binary "grain"
 end
