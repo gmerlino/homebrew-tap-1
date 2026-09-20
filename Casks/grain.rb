9cb298c310dc68a7538b91d819b28a3dff8db5c9
Giovanni Merlino <gmerlino@unime.it>
2026-09-20 23:53:10 +0200

    Remove redundant verified attribute from grain.rb
    Warning: Calling the `verified` parameter in the `url` stanza is deprecated! Use the default URL verification behaviour instead.
    Please report this issue to the grain-lang/homebrew-tap tap (not Homebrew/* repositories), or even better, submit a PR to fix it:
      /opt/homebrew/Library/Taps/grain-lang/homebrew-tap/Casks/grain.rb:5

--- Casks/grain.rb
+++ Casks/grain.rb
@@ -2,8 +2,7 @@ cask "grain" do
   version "0.7.2"
   sha256 "f7d53bf0b59d2eecf55dcb7df474d2bcf501aa5c83c74965a0ae57b2f9336750"
 
-  url "https://github.com/grain-lang/grain/releases/download/grain-v#{version}/grain-mac-x64",
-      verified: "github.com/grain-lang/grain/"
+  url "https://github.com/grain-lang/grain/releases/download/grain-v#{version}/grain-mac-x64"
   name "The Grain Programming Language"
   desc "Packaged Grain binary, including the compiler, runtime & stdlib"
   homepage "https://grain-lang.org/"
